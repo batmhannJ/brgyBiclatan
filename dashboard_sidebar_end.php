656d0f324a6de466b77643d3e47674d499a3d65c
batmhannJ <reyeshannahjoy82@gmail.com>
2024-05-04 21:20:09 +0800

    footer

--- dashboard_sidebar_end.php
+++ dashboard_sidebar_end.php
@@ -1,80 +1,101 @@
- <!-- Footer -->
-        <!-- Footer -->
+<!DOCTYPE html>
+<html lang="en">
 
-        <footer id="footer" style="background-color: #007bff; color: white;" class="d-flex-column text-center">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Responsive Footer</title>
 
-            <!--Copyright-->
+    <!-- Bootstrap CSS -->
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css">
 
-            <div class="py-3 text-center">
-                2023 -
-                <script>
-                document.write(new Date().getFullYear())
-                </script> 
-             | Barangay Biclatan Information System
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/67a9b7069e.js" crossorigin="anonymous"></script>
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper" class="d-flex flex-column" style="min-height: 100vh;">
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Scroll to Top Button-->
+                <a class="scroll-to-top rounded" href="#page-top">
+                    <i class="fas fa-angle-up"></i>
+                </a>
+
+                <!-- Logout Modal-->
+                <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+                    aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">×</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                            <div class="modal-footer">
+                                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                                <a class="btn btn-primary" href="logout.php">Logout</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
-
-        </footer>
-
-            <!-- End of Footer -->
+            <!-- End of Content -->
 
         </div>
         <!-- End of Content Wrapper -->
 
+        <!-- Footer -->
+        <footer id="footer" class="d-flex-column text-center" style="background-color: #007bff; color: white; width: 100%;">
+            <div class="py-3 text-center">
+                2023 -
+                <script>
+                    document.write(new Date().getFullYear())
+                </script>
+                | Barangay Biclatan Information System
+            </div>
+        </footer>
+        <!-- End of Footer -->
+
     </div>
     <!-- End of Page Wrapper -->
 
-    <!-- Scroll to Top Button-->
-    <a class="scroll-to-top rounded" href="#page-top">
-        <i class="fas fa-angle-up"></i>
-    </a>
-
-    <!-- Logout Modal-->
-    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
-                </div>
-                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-                <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="logout.php">Logout</a>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- Bootstrap core JavaScript-->
+    <!-- Bootstrap core JavaScript -->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-    <!-- Core plugin JavaScript-->
+    <!-- Core plugin JavaScript -->
     <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
 
-    <!-- Custom scripts for all pages-->
+    <!-- Custom scripts for all pages -->
     <script src="js/sb-admin-2.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-modal/2.2.6/js/bootstrap-modalmanager.min.js" integrity="sha512-/HL24m2nmyI2+ccX+dSHphAHqLw60Oj5sK8jf59VWtFWZi9vx7jzoxbZmcBeeTeCUc7z1mTs3LfyXGuBU32t+w==" crossorigin="anonymous"></script>
-    <!-- responsive tags for screen compatibility -->
-    <meta name="viewport" content="width=device-width, initial-scale=1 shrink-to-fit=no">
-    <!-- bootstrap css --> 
-    <link href="bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css"> 
-    <!-- fontawesome icons -->
-    <script src="https://kit.fontawesome.com/67a9b7069e.js" crossorigin="anonymous"></script>
-    <script src="bootstrap/js/bootstrap.bundle.js" type="text/javascript"> </script>
-
-    <!--<style>
-        #footer {
-            position: fixed;
-            bottom: 0;
-            width: 100%;
-        }
-    </style>-->
-
 </body>
 
 </html>
+
+<style>
+    html, body {
+        height: 100%;
+    }
+    #content-wrapper {
+        flex: 1;
+    }
+    #footer {
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        right: 0;
+    }
+</style>
