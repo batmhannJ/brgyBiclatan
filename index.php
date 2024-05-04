ecd425888d5de90c088c4757415605af85a25170
batmhannJ <reyeshannahjoy82@gmail.com>
2024-05-04 21:07:36 +0800

    new index

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <? php
     error_reporting(E_ALL ^ E_WARNING);
-    ini_set('display_errors',0);
+    ini_set('display_errors',1);
 
     //include('autoloader.php');
     require('classes/conn.php');
